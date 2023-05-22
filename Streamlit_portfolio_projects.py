1e758da1a6025a3be7d10d9745696c4c5592077b
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-22 19:12:09 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -105,7 +105,7 @@ if submitted:
     # Display charts for selected projects
     for project in selected_projects:
         st.header(f"Here's some charts for {project.capitalize()}!")
-        if project in {'vomex','imposible-finance'}:
+        if project in ['vomex','imposible-finance']:
             continue
         else:
             f = portfolio_projects_fdv_timeseries(project)
