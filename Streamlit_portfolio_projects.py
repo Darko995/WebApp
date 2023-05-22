06c548f8f9ab592511a185b748f6e73afa200141
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-22 17:23:29 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -37,16 +37,36 @@ def portfolio_projects_fdv_timeseries(project_id):
         df = df.T.dropna()
         return df
     
-    headers = {"Authorization": "Bearer 3365c8fd-ade3-410f-99e4-9c82d9831f0b"}
-    
-    fig, ax = plt.subplots(figsize=(24, 14))
-    
-    url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_fully_diluted"
-    response = requests.get(url, headers=headers)
-    data_shows = json.loads(response.text)
-    data = data_shows['data']
-    df = get_data(data)
-        
+    def get_data_c(data):
+        date = []
+        fdv  =[]
+        for i in range(len(data)):
+            date.append(pd.to_datetime((data[i]['timestamp'])))
+            FDV.append(data[i]['market_cap_circulating'])
+        dataa = [fdv]
+        df = pd.DataFrame(dataa, columns=date, index=['FDV'])
+        df = df.T.dropna()
+        return df
+    try :
+        headers = {"Authorization": "Bearer 3365c8fd-ade3-410f-99e4-9c82d9831f0b"}
+
+        fig, ax = plt.subplots(figsize=(24, 14))
+
+        url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_fully_diluted"
+        response = requests.get(url, headers=headers)
+        data_shows = json.loads(response.text)
+        data = data_shows['data']
+        df = get_data(data)
+    except KeyError:  
+        headers = {"Authorization": "Bearer 3365c8fd-ade3-410f-99e4-9c82d9831f0b"}
+
+        fig, ax = plt.subplots(figsize=(24, 14))
+
+        url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_circulating"
+        response = requests.get(url, headers=headers)
+        data_shows = json.loads(response.text)
+        data = data_shows['data']
+        df = get_data_c(data)
     df['fdv'].plot(color='crimson', ax=ax, label=f'{project_id} fdv')
     ax.set_title(f"FDV of {project_id}", fontsize=18)
     ax.set_xlabel('Date', fontsize=18)
