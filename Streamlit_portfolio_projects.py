ac6ca9f07bdfca2511c801fcbf235d27b78a8eff
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-24 20:23:48 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -490,11 +490,11 @@ if submitted:
         st.pyplot(tv)
         
 # ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
+#hide_st_style = """
+          #  <style>
+         #   #MainMenu {visibility: hidden;}
+          #  footer {visibility: hidden;}
+          #  header {visibility: hidden;}
+          #  </style>
+          #  """
 st.markdown(hide_st_style, unsafe_allow_html=True)
