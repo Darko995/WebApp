b5e336a1fc86c4a99a14f2d5bc66792726ed9b27
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-25 23:51:34 +0200

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -4,7 +4,7 @@ import streamlit_authenticator as stauth
 
 names = ["iosg"]
 usernames = ["iosgventures"]
-passwords = ["iosg 2023"]
+passwords = ["iosg2023"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
