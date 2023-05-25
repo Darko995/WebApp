fce05b531571e2fbda7d26cee9b5c0f94e80f8d0
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-25 09:18:21 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -15,8 +15,8 @@ st.set_page_config(page_title="Portfolio Priority projects", page_icon="🧐", l
 st.sidebar.header("Please Filter Timeseries Here:")
 chart = st.sidebar.multiselect(
     "Select the Metric:",
-    options=['FDV', 'MCAP', 'TVL', 'FEES','FEES/TVL','Tokenholders','Active Developers','Code Commits','Trading Volume'],
-    default=['FDV', 'MCAP', 'TVL', 'FEES','FEES/TVL','Tokenholders','Active Developers','Code Commits','Trading Volume']
+    options=['FDV', 'MCAP', 'TVL', 'FEES','FEES/TVL','Tokenholders','Active Developers','Code Commits','Trading Volume','Price'],
+    default=['FDV', 'MCAP', 'TVL', 'FEES','FEES/TVL','Tokenholders','Active Developers','Code Commits','Trading Volume','Price']
 )
 # ---- MAINPAGE ----
 st.title(":bar_chart: Portfolio Priority projects")
@@ -418,7 +418,44 @@ def portfolio_projects_trading_volume_timeseries(project_id, start_date,end_date
         ax.set_title(f"Sorry, there is no available data for {project_id} Trading volume!", fontsize=24)
 
         return fig 
+def portfolio_projects_price_timeseries(project_id, start_date,end_date):
       
+    def get_data_price(data):
+        date = []
+        price = []
+        for i in range(len(data)):
+            date.append(pd.to_datetime((data[i]['timestamp'])))
+            price.append(data[i]['price'])
+        dataa = [price]
+        df = pd.DataFrame(dataa, columns=date, index=['price'])
+        df = df.T.dropna()
+        return df
+    try:
+        headers = {"Authorization": st.secrets["APY_KEY"]}
+    
+        fig, ax = plt.subplots(figsize=(24, 14))
+    
+        url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=price"
+        response = requests.get(url, headers=headers)
+        data_shows = json.loads(response.text)
+        data = data_shows['data']
+        df = get_data_price(data)
+        df = df[f'{start_date}':f'{end_date}']
+      
+        df['price'].plot(color='crimson', ax=ax, label=f'{project_id} price')
+        ax.set_title(f"Price of {project_id}", fontsize=28)
+        ax.set_xlabel('Date', fontsize=18)
+        ax.set_ylabel('Price', fontsize=18)
+        ax.legend(loc='upper left', fontsize=14)
+        ax.legend(loc='upper right', fontsize=14)
+
+        return fig
+    except KeyError:
+        fig, ax = plt.subplots(figsize=(24, 4))
+
+        ax.set_title(f"Sorry, there is no available data for {project_id} Price!", fontsize=24)
+
+        return fig       
 columns = 3  # Number of columns
 selected_projects = []
 
@@ -497,13 +534,8 @@ if submitted:
             st.subheader("Token Trading Volume")
             tv = portfolio_projects_trading_volume_timeseries(project,start_date,end_date)
             st.pyplot(tv)
-        
-# ---- HIDE STREAMLIT STYLE ----
-#hide_st_style = """
-          #  <style>
-         #   #MainMenu {visibility: hidden;}
-          #  footer {visibility: hidden;}
-          #  header {visibility: hidden;}
-          #  </style>
-          #  """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
+        if 'Price' in chart:
+            st.subheader("Price")
+            pr = portfolio_projects_price_timeseries(project,start_date,end_date)
+            st.pyplot(pr)
+
