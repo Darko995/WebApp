523bb4a5304442e0773d108de044e4177275440c
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-26 19:06:21 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -302,7 +302,7 @@ elif authentication_status:
             d['fees'] = d['fees'].rolling(30).sum().dropna()
             d['fees'] = d['fees'] * (365 / 30)
             d['fees/tvl'] = d['fees']/d['TVL']
-            start_date = pd.Timestamp(start_date).tz_localize(d.index.tz)
+            #start_date = pd.Timestamp(start_date).tz_localize(d.index.tz)
             #if start_date > d.index[0]:
             d = d[f'{start_date}':f'{end_date}']
             # Resample the data to weekly frequency
