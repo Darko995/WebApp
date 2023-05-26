3db6231656677de55af543ac4af74463f9db4dac
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-26 09:27:58 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -87,20 +87,24 @@ elif authentication_status:
             data = data_shows['data']
             df = get_data(data)
             df = df[f'{start_date}':f'{end_date}']
-        except KeyError:  
-            headers = {"Authorization": st.secrets["APY_KEY"]}
-
-            fig, ax = plt.subplots(figsize=(24, 14))
-
-            url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_circulating"
-            response = requests.get(url, headers=headers)
-            data_shows = json.loads(response.text)
-            data = data_shows['data']
-            df = get_data_c(data)
-            df = df[f'{start_date}':f'{end_date}']
         except KeyError:
-        # Code to handle any other exception
-            pass
+            try:
+                headers = {"Authorization": st.secrets["APY_KEY"]}
+
+                fig, ax = plt.subplots(figsize=(24, 14))
+
+                url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_circulating"
+                response = requests.get(url, headers=headers)
+                data_shows = json.loads(response.text)
+                data = data_shows['data']
+                df = get_data_c(data)
+                df = df[f'{start_date}':f'{end_date}']
+            except KeyError:
+                fig, ax = plt.subplots(figsize=(24, 4))
+
+                ax.set_title(f"Sorry, there is no available data for {project_id} FDV!", fontsize=24)
+
+                return fig
 
         df['fdv'].plot(color='crimson', ax=ax, label=f'{project_id} fdv')
         ax.set_title(f"FDV of {project_id} from {start_date} to {end_date}", fontsize=28)
