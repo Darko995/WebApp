52f84af867370c57436aa0ed2aeeccb8f78f928c
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-05-26 09:32:53 +0200

    Update Streamlit_portfolio_projects.py

--- Streamlit_portfolio_projects.py
+++ Streamlit_portfolio_projects.py
@@ -87,6 +87,14 @@ elif authentication_status:
             data = data_shows['data']
             df = get_data(data)
             df = df[f'{start_date}':f'{end_date}']
+            df['fdv'].plot(color='crimson', ax=ax, label=f'{project_id} fdv')
+            ax.set_title(f"FDV of {project_id} from {start_date} to {end_date}", fontsize=28)
+            ax.set_xlabel('Date', fontsize=18)
+            ax.set_ylabel('FDV', fontsize=18)
+            ax.legend(loc='upper left', fontsize=14)
+            ax.legend(loc='upper right', fontsize=14)
+
+            return fig
         except KeyError:
             try:
                 headers = {"Authorization": st.secrets["APY_KEY"]}
@@ -99,6 +107,14 @@ elif authentication_status:
                 data = data_shows['data']
                 df = get_data_c(data)
                 df = df[f'{start_date}':f'{end_date}']
+                df['fdv'].plot(color='crimson', ax=ax, label=f'{project_id} fdv')
+                ax.set_title(f"FDV of {project_id} from {start_date} to {end_date}", fontsize=28)
+                ax.set_xlabel('Date', fontsize=18)
+                ax.set_ylabel('FDV', fontsize=18)
+                ax.legend(loc='upper left', fontsize=14)
+                ax.legend(loc='upper right', fontsize=14)
+
+                return fig
             except KeyError:
                 fig, ax = plt.subplots(figsize=(24, 4))
 
@@ -106,14 +122,6 @@ elif authentication_status:
 
                 return fig
 
-        df['fdv'].plot(color='crimson', ax=ax, label=f'{project_id} fdv')
-        ax.set_title(f"FDV of {project_id} from {start_date} to {end_date}", fontsize=28)
-        ax.set_xlabel('Date', fontsize=18)
-        ax.set_ylabel('FDV', fontsize=18)
-        ax.legend(loc='upper left', fontsize=14)
-        ax.legend(loc='upper right', fontsize=14)
-
-        return fig
     def portfolio_projects_mcap_timeseries(project_id, start_date, end_date):
 
         def get_data_mcap(data):
@@ -147,26 +155,41 @@ elif authentication_status:
             data = data_shows['data']
             df = get_data_mcap(data)
             df = df[f'{start_date}':f'{end_date}']
+            df['mcap'].plot(color='crimson', ax=ax, label=f'{project_id} mcap')
+            ax.set_title(f"MCAP of {project_id} from {start_date} to {end_date}", fontsize=28)
+            ax.set_xlabel('Date', fontsize=18)
+            ax.set_ylabel('MCAP', fontsize=18)
+            ax.legend(loc='upper left', fontsize=14)
+            ax.legend(loc='upper right', fontsize=14)
+
+            return fig
         except KeyError:
-            headers = {"Authorization": st.secrets["APY_KEY"]}
+            try:
+                headers = {"Authorization": st.secrets["APY_KEY"]}
 
-            fig, ax = plt.subplots(figsize=(24, 14))
+                fig, ax = plt.subplots(figsize=(24, 14))
 
-            url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_fully_diluted"
-            response = requests.get(url, headers=headers)
-            data_shows = json.loads(response.text)
-            data = data_shows['data']
-            df = get_data_mcap_c(data)
-            df = df[f'{start_date}':f'{end_date}']
+                url = f"https://api.tokenterminal.com/v2/projects/{project_id}/metrics?metric_ids=market_cap_fully_diluted"
+                response = requests.get(url, headers=headers)
+                data_shows = json.loads(response.text)
+                data = data_shows['data']
+                df = get_data_mcap_c(data)
+                df = df[f'{start_date}':f'{end_date}']
 
-        df['mcap'].plot(color='crimson', ax=ax, label=f'{project_id} mcap')
-        ax.set_title(f"MCAP of {project_id} from {start_date} to {end_date}", fontsize=28)
-        ax.set_xlabel('Date', fontsize=18)
-        ax.set_ylabel('MCAP', fontsize=18)
-        ax.legend(loc='upper left', fontsize=14)
-        ax.legend(loc='upper right', fontsize=14)
+                df['mcap'].plot(color='crimson', ax=ax, label=f'{project_id} mcap')
+                ax.set_title(f"MCAP of {project_id} from {start_date} to {end_date}", fontsize=28)
+                ax.set_xlabel('Date', fontsize=18)
+                ax.set_ylabel('MCAP', fontsize=18)
+                ax.legend(loc='upper left', fontsize=14)
+                ax.legend(loc='upper right', fontsize=14)
 
-        return fig
+                return fig
+            except KeyError:
+                fig, ax = plt.subplots(figsize=(24, 4))
+
+                ax.set_title(f"Sorry, there is no available data for {project_id} MCAP!", fontsize=24)
+
+                return fig
 
     def portfolio_projects_tvl_timeseries(project_id, start_date, end_date):
 
