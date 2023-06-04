437ad5dee7115cc4ec9d966ca86cbbd227d5caa5
Darko995 <53577768+Darko995@users.noreply.github.com>
2023-06-04 17:31:34 +0200

    Update Streamlit_token_vesting_management_next_month.py

--- Streamlit_token_vesting_management_next_month.py
+++ Streamlit_token_vesting_management_next_month.py
@@ -10,7 +10,7 @@ import pandas as pd
 import yfinance as yf
 import datetime 
 
-st.set_page_config(page_title="Token Vesting Management", page_icon="🧐", layout="wide")
+st.set_page_config(page_title="Token Vesting Management for next month", page_icon="🧐", layout="wide")
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
@@ -38,7 +38,7 @@ elif authentication_status:
     
     
     # ---- MAINPAGE ----
-    st.title(":bar_chart: Token Vesting Management")
+    st.title(":bar_chart: Token Vesting Management for next month")
     st.markdown("##")
     vesting_schedule_illuvium = {pd.Timestamp('2022-07-30'):8.33, pd.Timestamp('2022-08-30'):8.33, pd.Timestamp('2022-09-30'):8.33, pd.Timestamp('2022-10-30'):8.33,
                         pd.Timestamp('2022-11-30'):8.33, pd.Timestamp('2022-12-30'):8.33,pd.Timestamp('2023-01-30'):8.33,
